9359f7748a7bb51abbe2a4b67168dc1cc89088fe
Roberto Edgardo Perez Bertoli <91570445+robertoperez2793@users.noreply.github.com>
2022-11-14 11:38:27 -0300

    Add files via upload

---
+++ Ejercicios_openbootcamp/src/paquete_1/Clase_1.java
@@ -0,0 +1,25 @@
+package paquete_1;
+
+public class Clase_1 {
+
+	public static void main(String[] args) {
+		
+		int numero = 5;
+		long numeroLargo = 566646464;
+		float numeroFlotante = 10.5f;
+		double numeroDouble = 20.37d;
+		char caracter = 'a';
+		String cadena = "Hola soy una cadena";
+		boolean boleano1 = true;
+		
+		System.out.println(numero);
+		System.out.println(numeroLargo);
+		System.out.println(numeroFlotante);
+		System.out.println(numeroDouble);
+		System.out.println(caracter);
+		System.out.println(cadena);
+		System.out.println(boleano1);
+
+	}
+
+}
