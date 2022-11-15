9481524aeed23e2d2010043cc61f77a88c5a6b60
Roberto Edgardo Perez Bertoli <91570445+robertoperez2793@users.noreply.github.com>
2022-11-15 16:37:34 -0300

    Add files via upload

---
+++ Ejercicios_openbootcamp/ejercicio_concatenacion.java
@@ -0,0 +1,17 @@
+package paquete_1;
+
+public class ejercicio_concatenacion {
+
+	public static void main(String[] args) {
+		
+		String[] nombres = {"Roberto", "Pablo", "Marcelo", "Adrian", "Benito", "Marcos"};
+	
+		for(String i:nombres) {
+			System.out.println("Nombres: "+i);
+			
+	
+		}
+
+	}
+
+}
